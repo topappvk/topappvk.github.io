ef5b1e1e19394d1c25ce4e917fa6d4d49f6f5e20
topappvk <facevk.02@mail.ru>
2019-11-23 23:30:13 +0300

    counter relocate

---
+++ counter/precache-manifest.bf4fc82ee73e3d279d44da21d55f618f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2c0eb597afcebc59210",
+    "url": "/appcounter/static/js/runtime~main.53db829c.js"
+  },
+  {
+    "revision": "7f6e0efa818b838d0503",
+    "url": "/appcounter/static/js/main.12f74d7b.chunk.js"
+  },
+  {
+    "revision": "dc2ad512d674abe1c490",
+    "url": "/appcounter/static/js/2.ed8ef76e.chunk.js"
+  },
+  {
+    "revision": "7f6e0efa818b838d0503",
+    "url": "/appcounter/static/css/main.74fdcfa4.chunk.css"
+  },
+  {
+    "revision": "dc2ad512d674abe1c490",
+    "url": "/appcounter/static/css/2.611d5721.chunk.css"
+  },
+  {
+    "revision": "44cfaadb114b99d7e0f9e7ff1be897fd",
+    "url": "/appcounter/index.html"
+  }
+];
