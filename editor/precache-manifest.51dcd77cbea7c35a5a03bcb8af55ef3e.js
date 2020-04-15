8df358787d9b3435a1386e37f7dd33d067a53d1d
ciricc <facevk.02@mail.ru>
2020-04-15 19:29:45 +0300

    editor added

---
+++ editor/precache-manifest.51dcd77cbea7c35a5a03bcb8af55ef3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe2b05a090c2bd85d0d8d2e637f2c04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62db11ee888cb2f54839",
+    "url": "/static/css/main.d64ef517.chunk.css"
+  },
+  {
+    "revision": "c82d96b251c19196746b",
+    "url": "/static/js/2.8edf2c81.chunk.js"
+  },
+  {
+    "revision": "425801a503f2c75a3b68e630eb8c306c",
+    "url": "/static/js/2.8edf2c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62db11ee888cb2f54839",
+    "url": "/static/js/main.eef5cfcd.chunk.js"
+  },
+  {
+    "revision": "6c9424f29bb78d2eda65",
+    "url": "/static/js/runtime-main.40d0a053.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
